e97e6a27d732be941a59a4cde602d5bbd48e6c2c
github username <github email address>
2019-05-02 18:07:03 -0400

    changed airling code to airline name

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -13,19 +13,31 @@ class FlightsController < ApplicationController
   end
 
   def flight_search
-    url = URI.parse("https://api.skypicker.com/flights?flyFrom=#{params["start_location"]}&dateFrom=#{params["date"]}&dateTo=#{params["date"]}&price_to=#{params["price"]}&partner=picky"
+    airline_codes_url = URI.parse("https://api.skypicker.com/airlines"
+    )
+    airline_codes = Net::HTTP.get_response(airline_codes_url ).body
+    airline_codes_arr = JSON.parse(airline_codes)
+
+
+    flight_search_url = URI.parse("https://api.skypicker.com/flights?flyFrom=#{params["start_location"]}&dateFrom=#{params["date"]}&dateTo=#{params["date"]}&price_to=#{params["price"]}&partner=picky"
 )
-    response = Net::HTTP.get_response(url).body
-    flightsArr = JSON.parse(response)["data"]
+    flight_search_response = Net::HTTP.get_response(flight_search_url).body
+    flightsArr = JSON.parse(flight_search_response)["data"]
 
     if flightsArr
+
       finalArr = flightsArr.map do |flight|
+
+        airline_name = airline_codes_arr.find do |airline|
+          airline["id"] == flight["airlines"][0]
+        end
+
         flightHash = {}
         flightHash[:start_location] = flight["cityFrom"]
         flightHash[:start_airport] = flight["flyFrom"]
         flightHash[:end_airport] = flight["flyTo"]
         flightHash[:end_location] = flight["cityTo"]
-        flightHash[:airline] = flight["airlines"][0]
+        flightHash[:airline] = airline_name["name"]
         flightHash[:price] = flight["price"]
         flightHash[:departure_time] = Time.at(flight["dTime"])
         flightHash[:arrival_time] = Time.at(flight["aTime"])
@@ -36,7 +48,6 @@ class FlightsController < ApplicationController
     else
       render :json => ["invalid"]
     end
-
   end
 
   def flight_params
