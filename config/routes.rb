a98c6493c39d2ed634b6754fbfd956301877823a
github username <github email address>
2019-05-02 16:34:22 -0400

    added API call

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  resources :flights, only: [:index, :create] 
+  resources :flights, only: [:index, :create]
+  post '/flightsSearch', to: "flights#flight_search"
 end
