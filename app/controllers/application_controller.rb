73d39008e85bf55627ff9a2c717cad56a4503cd2
github username <github email address>
2019-05-08 15:21:46 -0400

    env key

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
     def encode_token(user_id)
-        JWT.encode({user_id: user_id}, "12345")
+        JWT.encode({user_id: user_id}, "#{ENV['LOGIN_KEY']}")
     end
 
     def get_token
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::API
 
     def decode_token
     begin
-        JWT.decode(get_token, "12345")[0]["user_id"]
+        JWT.decode(get_token, "#{ENV['LOGIN_KEY']}")[0]["user_id"]
     rescue
         # Token invalid, return nil
         nil
